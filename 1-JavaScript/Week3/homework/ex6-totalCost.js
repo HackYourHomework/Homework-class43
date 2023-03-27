1e039c1853ebc1797a4aedb6e060860d13096741
Valentyn Genkin <genkin.valentyn@gmail.com>
2023-03-27 19:11:41 +0200

    completed the 6 task

--- 1-JavaScript/Week3/homework/ex6-totalCost.js
+++ 1-JavaScript/Week3/homework/ex6-totalCost.js
@@ -1,42 +1,37 @@
+/* eslint-disable no-restricted-syntax */
 'use strict';
-/*------------------------------------------------------------------------------
-Full description at: https://github.com/HackYourFuture/Homework/tree/main/1-JavaScript/Week3#exercise-6-total-cost-is
 
-You want to buy a couple of things from the supermarket to prepare for a party.
-After scanning all the items the cashier wants to give you the total price, but
-the machine is broken! Let's write her a function that does it for her
-instead!
-
-1. Create an object named `cartForParty` with five properties. Each property
-   should be a grocery item (like `beers` or `chips`) and hold a number value
-   (like `1.75` or `0.99`).
-
-2. Complete the function called `calculateTotalPrice`.
-
-   - It takes one parameter: an object that contains properties that only contain
-     number values.
-   - Loop through the object and add all the number values together.
-   - Return a string: "Total: €`amount`".
-
-3. Complete the unit test functions and verify that all is working as expected.
------------------------------------------------------------------------------*/
 const cartForParty = {
-  // TODO complete this object
+  beers: 1.75,
+  chips: 0.99,
+  coke: 1.2,
+  water: 0.75,
+  nuts: 4.3,
 };
 
-function calculateTotalPrice(/* TODO parameter(s) go here */) {
-  // TODO replace this comment with your code
+function calculateTotalPrice(object) {
+  let amount = 0;
+  for (const price in object) {
+    if (typeof object[price] === 'number') {
+      amount += object[price];
+    }
+  }
+  return `Total: €${amount}`;
 }
 
 // ! Test functions (plain vanilla JavaScript)
 function test1() {
   console.log('\nTest 1: calculateTotalPrice should take one parameter');
-  // TODO replace this comment with your code
+  const prediction1 = calculateTotalPrice.length === 1;
+  console.assert(prediction1);
 }
 
 function test2() {
   console.log('\nTest 2: return correct output when passed cartForParty');
-  // TODO replace this comment with your code
+  console.log(calculateTotalPrice(cartForParty));
+  const prediction2 =
+    calculateTotalPrice(cartForParty) !== undefined || NaN || null;
+  console.assert(prediction2);
 }
 
 function test() {
