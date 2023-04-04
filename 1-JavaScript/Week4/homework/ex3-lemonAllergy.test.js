416b5ba311cd34fc5ad6487ebfee92ceef1a36a4
Valentyn Genkin <genkin.valentyn@gmail.com>
2023-04-04 10:44:40 +0300

    finished the 3 task

--- 1-JavaScript/Week4/homework/ex3-lemonAllergy.test.js
+++ 1-JavaScript/Week4/homework/ex3-lemonAllergy.test.js
@@ -26,26 +26,29 @@ const fruitBasket = [
 ];
 
 // ! Function under test
-function sanitizeFruitBasket(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+function sanitizeFruitBasket(array, dangerFruit) {
+  const newArray = array.filter(fruit => fruit !== dangerFruit)
+  return newArray
 }
 
 // ! Unit tests (using Jest)
 describe('sanitizeFruitBasket', () => {
   test('should take two parameters', () => {
     // TODO replace next line with your code
-    expect(false).toBe(true);
+    expect(sanitizeFruitBasket).toHaveLength(2);
   });
 
   test('should not modify the original `fruitBasket` array', () => {
     // Save the original contents of the fruit basket
     const originalFruitBasketContents = [...fruitBasket];
     // TODO replace next line with your code
-    expect(false).toBe(true);
+    const actual = fruitBasket;
+    expect(actual).toStrictEqual(originalFruitBasketContents);
   });
 
   test('should return a new array that does not include the unwanted `lemon`', () => {
     // TODO replace next line with your code
-    expect(false).toBe(true);
+    const newArr = sanitizeFruitBasket(fruitBasket, 'lemon');
+    expect(newArr.includes('lemon')).toBe(false);
   });
 });
