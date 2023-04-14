462974bfb5f2e1e85af9b9efab7d7af44c6d326a
EuvhenRight <ugnivenko.ea@gmail.com>
2023-04-15 00:36:45 +0200

    second  task browser done

--- 2-Browsers/Week1/homework/ex2-aboutMe/index.js
+++ 2-Browsers/Week1/homework/ex2-aboutMe/index.js
@@ -8,5 +8,23 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/2-Brow
    `list-item`.
 3. Look in the css file!
 ------------------------------------------------------------------------------*/
+// Create span query selector
+const nickname = document.querySelector('#nickname');
+// new content of span
+nickname.textContent = 'YenhenRight';
+// Create span query selector
+const favFood = document.querySelector('#fav-food');
+// new content of span
+favFood.textContent = 'Pasta with cheese';
+// Create span query selector
+const hometown = document.querySelector('#hometown');
+// new content of span
+hometown.textContent = 'Amsterdam';
 
-// TODO add your JavaScript code here.
+//Create query selectorALL const listItems
+const listItems = document.querySelectorAll('li');
+
+// Iterate through listItems and change class
+listItems.forEach((list) => {
+  list.className = 'list-item';
+});
