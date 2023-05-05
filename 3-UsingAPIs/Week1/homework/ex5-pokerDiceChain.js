5a7422ad4e01560a1f4acd151b4594d11270e12a
EuvhenRight <ugnivenko.ea@gmail.com>
2023-05-05 15:39:00 +0200

    All tasks done

--- 3-UsingAPIs/Week1/homework/ex5-pokerDiceChain.js
+++ 3-UsingAPIs/Week1/homework/ex5-pokerDiceChain.js
@@ -17,14 +17,25 @@ const rollDie = require('../../helpers/pokerDiceRoller');
 function rollDice() {
   const results = [];
 
-  // TODO: expand the chain to include five dice
   return rollDie(1)
     .then((value) => {
       results.push(value);
       return rollDie(2);
     })
-    .then((value) => {
-      results.push(value);
+    .then((value2) => {
+      results.push(value2);
+      return rollDie(3);
+    })
+    .then((value3) => {
+      results.push(value3);
+      return rollDie(4);
+    })
+    .then((value4) => {
+      results.push(value4);
+      return rollDie(5);
+    })
+    .then((value5) => {
+      results.push(value5);
       return results;
     });
 }
