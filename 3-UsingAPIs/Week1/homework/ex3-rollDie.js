1e387c41ba553a103896b4b2d30b52cfbecd1276
Samrawit Girma <samrawitg39333.eg@gmail.com>
2023-05-12 19:14:13 +0200

    api homework

--- 3-UsingAPIs/Week1/homework/ex3-rollDie.js
+++ 3-UsingAPIs/Week1/homework/ex3-rollDie.js
@@ -11,52 +11,60 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/3-Usin
   explanation? Add your answer as a comment to be bottom of the file.
 ------------------------------------------------------------------------------*/
 
-// TODO Remove callback and return a promise
-function rollDie(callback) {
-  // Compute a random number of rolls (3-10) that the die MUST complete
-  const randomRollsToDo = Math.floor(Math.random() * 8) + 3;
-  console.log(`Die scheduled for ${randomRollsToDo} rolls...`);
+function rollDie() {
+  return new Promise((resolve, reject) => {
+    const randomRollsToDo = Math.floor(Math.random() * 8) + 3;
+    console.log(`Die scheduled for ${randomRollsToDo} rolls...`);
 
-  const rollOnce = (roll) => {
-    // Compute a random die value for the current roll
-    const value = Math.floor(Math.random() * 6) + 1;
-    console.log(`Die value is now: ${value}`);
+    const rollOnce = (roll) => {
+      const value = Math.floor(Math.random() * 6) + 1;
+      console.log(`Die value is now: ${value}`);
 
-    // Use callback to notify that the die rolled off the table after 6 rolls
-    if (roll > 6) {
-      // TODO replace "error" callback
-      callback(new Error('Oops... Die rolled off the table.'));
-    }
+      // Use callback to notify that the die rolled off the table after 6 rolls
+      if (roll > 6) {
+        reject(new Error('Oops... Die rolled off the table.'));
+      }
 
-    // Use callback to communicate the final die value once finished rolling
-    if (roll === randomRollsToDo) {
-      // TODO replace "success" callback
-      callback(null, value);
-    }
+      // Use callback to communicate the final die value once finished rolling
+      if (roll === randomRollsToDo) {
+        resolve(value);
+      }
 
-    // Schedule the next roll todo until no more rolls to do
-    if (roll < randomRollsToDo) {
-      setTimeout(() => rollOnce(roll + 1), 500);
-    }
-  };
+      // Schedule the next roll todo until no more rolls to do
+      if (roll < randomRollsToDo) {
+        setTimeout(() => rollOnce(roll + 1), 500);
+      }
+    };
 
-  // Start the initial roll
-  rollOnce(1);
+    // Start the initial roll
+    rollOnce(1);
+  });
 }
 
 function main() {
-  // TODO Refactor to use promise
-  rollDie((error, value) => {
-    if (error !== null) {
-      console.log(error.message);
-    } else {
+  rollDie()
+    .then((value) => {
       console.log(`Success! Die settled on ${value}.`);
-    }
-  });
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
 }
 
 // ! Do not change or remove the code below
 if (process.env.NODE_ENV !== 'test') {
   main();
 }
+
+function wasteTimeBlocking() {
+  for (let count = 1; count <= 1000; count++) {
+    console.log('  count =', count);
+  }
+}
+wasteTimeBlocking();
 module.exports = rollDie;
+
+/*To solve this problem, one solution is to avoid blocking the event loop by either making wasteTimeBlocking() 
+asynchronous or using a non-blocking method to waste time, such as setTimeout(). 
+Another solution is to wrap the call to rollDie() in an immediately invoked async function and await its result, 
+which would allow the event loop to continue while the die is rolling. */
--- 3-UsingAPIs/Week1/homework/ex4-pokerDiceAll.js
+++ 3-UsingAPIs/Week1/homework/ex4-pokerDiceAll.js
@@ -27,9 +27,15 @@ exercise file.
 const rollDie = require('../../helpers/pokerDiceRoller');
 
 function rollDice() {
-  // TODO Refactor this function
   const dice = [1, 2, 3, 4, 5];
-  return rollDie(1);
+  const promises = dice.map((die) => rollDie(die));
+  return Promise.all(promises)
+    .then((results) => {
+      return results;
+    })
+    .catch((error) => {
+      throw error;
+    });
 }
 
 function main() {
@@ -43,3 +49,9 @@ if (process.env.NODE_ENV !== 'test') {
   main();
 }
 module.exports = rollDice;
+
+/*In the case of a rejected promise, the Promise.all() method immediately rejects with the error and
+ any pending promises are terminated. However, the rollDie() function uses asynchronous code that has already been dispatched, 
+ so those asynchronous operations may continue to execute even after the rejection has occurred. 
+ Therefore, the resolved or rejected status of a promise does not affect the asynchronous operations that have already been dispatched, 
+they will continue to run until they are completed or rejected.*/
