2fd8aaca32d5d7d247ac6b705986aba5bd584ccb
Muhie Aldeen <zezo1998@hotmail.com>
2023-05-13 11:50:50 +0200

    I got rid of the isDancing flag and its logice the codestill working

--- 2-Browsers/Week1/homework/ex5-catWalk/index.js
+++ 2-Browsers/Week1/homework/ex5-catWalk/index.js
@@ -25,36 +25,36 @@ const DANCING_CAT_URL =
   'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
 const cat = document.querySelector('img');
 
-let isDancing = false;
+// let isDancing = false;
 
 function catWalk() {
   let position = -cat.offsetWidth;
   cat.style.left = `${position}px`;
   const middle = (window.innerWidth - cat.offsetWidth) / 2;
   let catInterval = setInterval(() => {
-    if (!isDancing) {
-      cat.style.left = `${position}px`;
-      position += 10;
-    }
+    // if (!isDancing) {
+    cat.style.left = `${position}px`;
+    position += 10;
+    // }
 
     if (position >= middle) {
       clearInterval(catInterval);
-      isDancing = true;
-      if (isDancing) {
-        cat.src = DANCING_CAT_URL;
-        setTimeout(() => {
-          isDancing = false;
-          cat.src = 'http://www.anniemation.com/clip_art/images/cat-walk.gif';
-          catInterval = setInterval(() => {
-            cat.style.left = `${position}px`;
-            position += 10;
-            if (position > window.innerWidth) {
-              clearInterval(catInterval);
-              catWalk();
-            }
-          }, 50);
-        }, 5000);
-      }
+      // isDancing = true;
+      // if (isDancing) {
+      cat.src = DANCING_CAT_URL;
+      setTimeout(() => {
+        // isDancing = false;
+        cat.src = 'http://www.anniemation.com/clip_art/images/cat-walk.gif';
+        catInterval = setInterval(() => {
+          cat.style.left = `${position}px`;
+          position += 10;
+          if (position > window.innerWidth) {
+            clearInterval(catInterval);
+            catWalk();
+          }
+        }, 50);
+      }, 5000);
+      // }
     }
   }, 50);
 }
