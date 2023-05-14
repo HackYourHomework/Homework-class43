64e2211e57c92b982d79e3e74ae02b1a05176461
Mohamad Alamin <alamin.mohamad.a0@gmail.com>
2023-05-14 14:02:32 +0200

    Done with ex5

--- 2-Browsers/Week1/homework/ex5-catWalk/index.js
+++ 2-Browsers/Week1/homework/ex5-catWalk/index.js
@@ -1,4 +1,5 @@
 'use strict';
+
 /*------------------------------------------------------------------------------
 Full description at: https://github.com/HackYourFuture/Homework/tree/main/2-Browsers/Week1#exercise-5-the-cat-walk
 
@@ -21,19 +22,32 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/2-Brow
 
    https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif
 -----------------------------------------------------------------------------*/
-const catImg = document.querySelector("img");
-catImg.style.left = "0px"
+const catImg = document.querySelector('img');
+catImg.style.left = '0px';
 
 function catWalk() {
-let leftPosition = parseInt(cat.style.left)
+  let leftPosition = 0;
+  let catInterval = setInterval(() => {
+    catImg.style.left = `${leftPosition}px`;
+    leftPosition += 10;
 
-leftPosition += 10
-cat.style.left = leftPosition + "px"
-
+    if (leftPosition >= window.innerWidth / 2) {
+      clearInterval(catInterval);
+      catImg.src =
+        'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
+      setTimeout(() => {
+        catImg.src = 'http://www.anniemation.com/clip_art/images/cat-walk.gif';
+        catInterval = setInterval(() => {
+          catImg.style.left = `${leftPosition}px`;
+          leftPosition += 10;
+          if (leftPosition > window.innerWidth) {
+            clearInterval(catInterval);
+            catWalk();
+          }
+        }, 50);
+      }, 5000);
+    }
+  }, 50);
 }
 
-setInterval(catWalk, 50)
-
-// TODO execute `catWalk` when the browser has completed loading the page
-
-window.onload = catWalk
+window.onload = catWalk;
